96423595aa509be95d5927c222d48b1a24733cda
jas3 <36154250+Jaumeavinyo@users.noreply.github.com>
2022-06-27 18:42:54 +0200

    SDLManager created
    nothing added

---
+++ EasyNeuralNetworksProjectSolution/EasyNeuralNetworksProject/SDLManager.h
@@ -0,0 +1,31 @@
+#ifndef SDL_MANAGER_H_
+#define SDL_MANAGER_H_
+
+#include "SDL.h"
+
+class SDLManager
+{
+
+
+
+
+public:
+	SDLManager();
+	~SDLManager();
+
+
+
+	
+
+};
+
+SDLManager::SDLManager()
+{
+}
+
+SDLManager::~SDLManager()
+{
+}
+
+
+#endif
